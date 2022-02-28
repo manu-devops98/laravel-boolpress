d337155aeac579ba0ec8e8818cce28cd1f2049a6
manu-devops98 <manuelcappello7@gmail.com>
2022-02-28 13:19:02 +0100

    add seed,model and migration

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Model\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 30; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->word();
+            $newPost->content = $faker->paragraphs(4, true);
+            $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
+            $newPost->save();
+        }
+    }
+}

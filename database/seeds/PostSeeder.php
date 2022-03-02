909d47bb05d1f07f5b7d84c32f0f49c19a2eb706
manu-devops98 <manuelcappello7@gmail.com>
2022-03-02 14:38:36 +0100

    add migrations and seed on DB

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Model\Post;
+use App\Model\Category;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -21,6 +22,7 @@ class PostSeeder extends Seeder
             $newPost->content = $faker->paragraphs(4, true);
             $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
             $newPost->user_id = User::inRandomOrder()->first()->id;
+            $newPost->category_id = Category::inRandomOrder()->first()->id;
             $newPost->save();
         }
     }

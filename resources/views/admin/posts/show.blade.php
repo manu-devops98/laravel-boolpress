c01bf26f269c0c8c4dc7ebdf6cf3efd2a6e1d056
manu-devops98 <manuelcappello7@gmail.com>
2022-03-07 16:18:03 +0100

    add modifiche show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -27,12 +27,14 @@
                         @endforeach
                     </ul>
                 </div>
+                @if (!empty($post->image))
                 <div class="col-4">
                     <div class="container">
                         <h2 class="fs-3 text-danger">Image:</h2>
                         <img class="img-fluid" src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title}}">
                     </div>
                 </div>
+                @endif
             </div>
             {{-- <a class="btn btn-danger my-3" href="{{ route('admin.posts.index')}}">Home</a>
             <a href="{{ url()->previous()}}" type="submit" class="btn btn-primary">Previous</a> --}}

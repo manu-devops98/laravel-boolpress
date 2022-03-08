29dde9b1a47d9080f4a7ec7ce971c6022831225c
manu-devops98 <manuelcappello7@gmail.com>
2022-03-08 20:08:49 +0100

    add modifiche main e immagine di default

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col" v-for="(post, index) in posts" :key="index">
-            <div class="card">
-                <img src="" class="card-img-top" alt="" />
-                <div class="card-body">
-                    <h5 class="card-title">{{ post.title }}</h5>
-                    <p class="card-text">
-                        {{ post.content }}
-                    </p>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col" v-for="(post, index) in posts" :key="index">
+                <div class="card">
+                    <img
+                        :src="
+                            require('../../../storage/app/public/uploads/products/default.png')
+                        "
+                        class="card-img-top w-100"
+                        :alt="post.title"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        <p class="card-text">
+                            {{ post.content }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

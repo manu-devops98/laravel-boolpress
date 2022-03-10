2646c0cdc5c43850f50664a291e7c3eb7fe80c96
manu-devops98 <manuelcappello7@gmail.com>
2022-03-10 16:47:59 +0100

    add rotta posts e about

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>About us</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About",
+};
+</script>
+
+<style></style>
---
+++ resources/js/pages/Posts.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="">All Posts</h1>
+            </div>
+        </div>
+        <Main :cards="cards" @changePage="changePage($event)"></Main>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import Main from "../components/Main.vue";
+export default {
+    name: "Posts",
+    components: {
+        Main,
+    },
+    data() {
+        return {
+            cards: {
+                posts: null,
+                nextPage: null,
+                prevPage: null,
+            },
+        };
+    },
+    methods: {
+        getPosts(url) {
+            axios.get(url).then((result) => {
+                this.cards.posts = result.data.results.posts.data;
+                this.cards.nextPage = result.data.results.posts.next_page_url;
+                this.cards.prevPage = result.data.results.posts.prev_page_url;
+            });
+        },
+        changePage(change) {
+            let url = this.cards[change];
+            if (url) {
+                this.getPosts(url);
+            }
+        },
+    },
+    created() {
+        this.getPosts("http://127.0.0.1:8000/api/posts");
+    },
+};
+</script>
+
+<style></style>
